dcd0ff1cfb69e46d34d7769fc96d446b5f372b47
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-09-29 10:43:47 +0200

    Add Constructor, Getter/Setter and hasCode/equals

--- src/main/java/org/example/school/Student.java
+++ src/main/java/org/example/school/Student.java
@@ -1,7 +1,54 @@
-package org.example;
+package org.example.school;
+
+import java.util.Objects;
 
 public class Student {
-    private String Vorname;
-    private String Nachname;
-    private String Matrikelnummer;
+    private String firstName;
+    private String lastName;
+    private String 	matriculationNumber;
+
+    public Student(String firstName, String lastName, String matriculationNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.matriculationNumber = matriculationNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMatriculationNumber() {
+        return matriculationNumber;
+    }
+
+    public void setMatriculationNumber(String matriculationNumber) {
+        this.matriculationNumber = matriculationNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(matriculationNumber, student.matriculationNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, matriculationNumber);
+    }
 }
+
+
