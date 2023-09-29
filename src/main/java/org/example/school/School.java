a35ae3e5a1dd8792e27e577940659991de124634
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-09-29 11:02:41 +0200

    Add class "School"

---
+++ src/main/java/org/example/school/School.java
@@ -0,0 +1,40 @@
+package org.example.school;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class School {
+    List<Student> studentsOfSchool = new ArrayList<>();
+
+    public School() {
+    }
+
+    public List<Student> getStudentsOfSchool() {
+        return studentsOfSchool;
+    }
+
+    public void setStudentsOfSchool(List<Student> studentsOfSchool) {
+        this.studentsOfSchool = studentsOfSchool;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        School school = (School) o;
+        return Objects.equals(studentsOfSchool, school.studentsOfSchool);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentsOfSchool);
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "studentsOfSchool=" + studentsOfSchool +
+                '}';
+    }
+}
